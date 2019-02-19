57aee8ed5b78c5dd6d4c409d1c18e60be93dd43f
jlucasd01 <jlucasd01@gmail.com>
2019-02-19 18:19:01 -0300

    Heroku works

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:appdoape
+web: python app.py
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from datetime import datetime
 
@@ -43,5 +44,7 @@ def getResidents():
     return ['Julio', 'Marcos', 'João']
 
 if __name__ == '__main__':
-    print("server is runing!!")
-    app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # print("server is runing!!")
+    # app.run(debug=True)
