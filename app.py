ae011241fb9e6bf40fc78f5c1750eed24aa58173
jlucasd01 <jlucasd01@gmail.com>
2019-03-09 11:21:16 -0300

    Novas alterações pdf

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ def getResidents():
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
-	pp.run(host='0.0.0.0', port=port)
-    #  print("server is runing!!")
-    #  app.run(debug=True)
+	app.run(host='0.0.0.0', port=port)
+    # print("server is runing!!")
+    # app.run(debug=True)
