acb0e07fbc05dcb42913c67e78ad8399fcc0a9a0
João Damiani <jlucasd01@gmail.com>
2020-02-16 11:49:05 -0300

    Nova página para ver o  caixa do apê

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_login import login_required
 from datetime import datetime
 from forms import FormLogin
 from config import Config
 from database.base import Base
 import sqlite3
+from dotenv import load_dotenv 
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -27,7 +29,7 @@ def index():
 def login():
     form = FormLogin()
     if form.validate_on_submit():
-        if((form.user.data == 'apsenai') and (form.password.data == 'apsenai')):
+        if((form.user.data == 'apesenai') and (form.password.data == 'apesenai')):
             session['logged_in'] = True
             return redirect(url_for('index'))
     return render_template('login.html', form=form)
@@ -38,12 +40,21 @@ def logout():
     return index()
 
 @app.route('/pagamentos')
+#@login_required
 def pagamentos():
     return render_template('pagamentos.html', payers=getPayers(), columns=getColumns(), year=getCurrentYear())
 
 @app.route('/contas',methods = ['POST', 'GET'])
+#@login_required
 def contas():
     return render_template('contas.html', residents=getResidents(), currentPayer=getCurrentPayer())
+
+@app.route('/caixa',methods = ['POST', 'GET'])
+#@login_required
+def caixa():
+    meses = diff_month(datetime(2010,10,1), datetime(2010,9,1))
+    valor = meses * 20
+    return render_template('caixa.html', valor=valor)
 
 def getMonths():
     return ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho","Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
@@ -72,9 +83,12 @@ def getUsers():
         'marcos': 'profile.php?id=100002460771942' 
     }
 
+def diff_month(d1, d2):
+    return (d1.year - d2.year) * 12 + d1.month - d2.month
+
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port)
     # print("server is runing!!")
-    # app.run(debug=True)
+    app.run(debug=True)
